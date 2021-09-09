ca3ab46328e6a500d2bf7a53d21746e68e6ff7c4
Z0005HSM <lothar.becke@siemens.com>
2021-09-09 13:11:55 +0200

    Point leg C now can leave the bounding box at top/bottom also. Introduced new layout parameter length_legA.

--- PointWidget.py
+++ PointWidget.py
@@ -7,33 +7,47 @@ class PointWidget(TrackWidgetBase):
     def __init__(self, *, ori, height, width, **placement):
         super().__init__(ori=ori, width=width, height=height, **placement)
 
+    def idle_xy (self, x_or_y : int) -> int:
+        return x_or_y
+
+    def mirror_x(self, x : int) -> int:
+        return self.width - x
+
+    def mirror_y(self, y : int) -> int:
+        return self.height - y
+
+    def mk_coords(self, func_x, func_y):
+        self.pa = Coord(func_x(0), func_y(self.height-10))
+        self.pb = Coord(func_x(self.width), func_y(self.height-10))
+        self.p = Coord(func_x(self.length_legA), func_y(self.height-10))
+        if self.width - self.length_legA > self.height - 10:
+            # connector C at top/bottom
+            self.pc = Coord(func_x(self.length_legA + self.height - 10), func_y(0))
+        else:
+            # connector C at left/right
+            self.pc = Coord(func_x(self.width), func_y(self.height - 10 - self.width + self.length_legA))
+
     def create_widget(self, name, master_frame, gui):
         TrackWidgetBase.create_widget(self, name, master_frame, gui)
+        self.length_legA = self.placement.get('length_legA', self.width-self.height+20)
 
         if self.ori == 'ne':
-            pa = Coord(0, self.height - SCALE)
-            pb = Coord(self.width, self.height - SCALE)
-            pc = Coord(self.width, SCALE)
-            p = Coord(self.width - self.height + 2 * SCALE, self.height - SCALE)
+            self.mk_coords(self.idle_xy, self.idle_xy)
             text_y_default = self.height - 4
         if self.ori == 'nw':
-            pa = Coord(self.width, self.height - SCALE)
-            pb = Coord(0, self.height - SCALE)
-            pc = Coord(0, SCALE)
-            p = Coord(self.height-2*SCALE, self.height - SCALE)
+            self.mk_coords(self.mirror_x, self.idle_xy)
             text_y_default = self.height - 4
         if self.ori == 'se':
-            pa = Coord(0, SCALE)
-            pc = Coord(self.width, 1 * SCALE)
-            pb = Coord(self.width, self.height - SCALE)
-            p = Coord(self.width - self.height + 2 * SCALE, SCALE)
+            self.mk_coords(self.idle_xy, self.mirror_y)
             text_y_default = 4
         if self.ori == 'sw':
-            pa = Coord(self.width, SCALE)
-            pc = Coord(0, 1 * SCALE)
-            pb = Coord(0, self.height - SCALE)
-            p = Coord(self.height - 2 * SCALE, SCALE)
+            self.mk_coords(self.mirror_x, self.mirror_y)
             text_y_default = 4
+
+        pa = self.pa
+        pb = self.pb
+        p = self.p
+        pc = self.pc
 
         self.legA = self.canvas.create_line(pa.x, pa.y, p.x, p.y, width=4, fill='yellow', capstyle='butt')
         self.legB = self.canvas.create_line(p.x, p.y, pb.x, pb.y, width=4, fill='yellow', capstyle='butt')
