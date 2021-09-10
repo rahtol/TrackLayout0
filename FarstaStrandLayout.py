9602edc6a65bf415a65d0ca0c91abe30ac5d3e7c
Z0005HSM <lothar.becke@siemens.com>
2021-09-10 16:35:58 +0200

    Sheet #2 of area Gubbängen complete. Added horizontal scrolling.

--- FarstaStrandLayout.py
+++ FarstaStrandLayout.py
@@ -18,8 +18,19 @@ def get_loader():
 class FarstaStrandGui:
     def __init__(self, master):
         self.master = master
-        self.frame = tk.Frame(self.master, height=400, width=1600)
-        self.frame.pack()
+        self.frame0 = tk.Frame(self.master, height=400, width=1900)
+
+        self.canvas = tk.Canvas(self.frame0, height=400, width=1900, highlightthickness=0)
+        self.frame = tk.Frame(self.canvas, height=400, width=3900)
+        self.hsb = tk.Scrollbar(self.frame0, orient="horizontal", command=self.canvas.xview)
+        self.canvas.configure(xscrollcommand=self.hsb.set)
+        self.hsb.pack(side="bottom", fill="x")
+        self.canvas.pack(side="top", fill="both", expand=True)
+        self.canvas.create_window((0,0), window=self.frame, anchor="nw", tags="self.frame")
+        self.frame.bind("<Configure>", self.onFrameConfigure)
+        self.frame0.pack()
+#        self.canvas.xview_moveto(0.4)
+
         master.title("Farsta Strand Gui #2")
 
         stream = open("FarstaStrandLayout.yaml", 'r')
@@ -31,6 +42,9 @@ class FarstaStrandGui:
         for widget in display_widgets.values():
             widget.place_widget(display_widgets)
 
+    def onFrameConfigure(self, event):
+        '''Reset the scroll region to encompass the inner frame'''
+        self.canvas.configure(scrollregion=self.canvas.bbox("all"))
 
 root = tk.Tk()
 gui = FarstaStrandGui(root)
